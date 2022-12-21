1fd713916a6b05e82da17cbe5b5af58efeaec633
XibalbaM <maelporret@outlook.fr>
2022-12-21 22:37:37 +0100

    Suppression d'une variable devenue inutile.

--- sources/javascript/character/create.js
+++ sources/javascript/character/create.js
@@ -29,19 +29,6 @@ NJCControllers.controller('CharacterCreate', [
     $rootScope.pageColor = '#947bff';
     $rootScope.pageName = 'njc-page-create';
 
-    var trackingData = {
-      "page": $location.url()
-    };
-
-    if (typeof ($routeParams.who) === 'undefined') {
-      sUser.reset();
-      trackingData.title = 'Création';
-    } else {
-      sUser.setCurrent($routeParams.who);
-      trackingData.title = 'Edition';
-      trackingData.dimension1 = sUser.get().clan;
-    }
-
     sNinjaRank.load().then(function (data) {
       $scope.ninjaRanks = data;
     });
