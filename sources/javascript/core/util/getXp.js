d78f783a4e291b88fee16e5871eb4693b48bb8f8
XibalbaM <maelporret@outlook.fr>
2023-01-02 20:19:18 +0100

    Correction du bug : l'expérience décroit qu'à partir de la sixième caractéristiques augmentées.

--- sources/javascript/core/util/getXp.js
+++ sources/javascript/core/util/getXp.js
@@ -1,31 +1,24 @@
+const basesXpCost = [0, 15, 35, 60, 90, 125, 165, 210, 260, 315, 375, 440, 510, 585, 675, 775];
+
 /**
  * A utility function to get the amount of xp used in skills and bases.
- * @param input The character object to get the xp from.
+ * @param {Object} input The character object to get the xp from.
  * @returns {number} The amount of xp used.
  */
-var getXp = function (input) {
+const getXp = function (input) {
 
-    var total = 0;
+    let total = 0;
 
-    for (var base in input.bases) {
-        var levelBase = input.bases[base] || 1;
-        while (levelBase > 1) {
-            total += (5 + levelBase * 5);
-            levelBase--;
-        }
+    for (const base in input.bases) {
+        total += basesXpCost[(input.bases[base] - 1) || 0];
     }
 
-    for (var skill in input.skills) {
-        var levelSkill = input.skills[skill] || 1;
-        while (levelSkill > 1) {
-            total += levelSkill;
-            levelSkill--;
-        }
+    for (const skill in input.skills) {
+        let levelSkill = input.skills[skill] || 1;
+        total += (levelSkill * levelSkill + levelSkill - 2) / 2;
     }
 
-    total -= 35 * 3;
     total = Math.max(total, 0);
 
     return input.xp - total;
-
 };
